72a8f2b30b976154fca2dc2765522339d98ada5b
halvardssm <halvard@simplyundoable.com>
2020-10-09 21:56:05 +0200

    updated storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const custom = require('../webpack.config.js');
 
+custom.module.rules.splice(0,1)
+
 module.exports = {
   stories: ['../src/components/**/*.stories.[tj]s'],
   addons: ['@storybook/addon-knobs/register'],
   webpackFinal: async (config, { configType }) => {
-    return { ...config, module: { ...config.module, rules: custom.module.rules } };
+    return { ...config, module: { ...config.module, rules: [...config.module.rules,...custom.module.rules] } };
   },
 };

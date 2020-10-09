6250169a635be9600fa3a4d03899b741e321b3fa
leifssm <lofftull@gmail.com>
2020-10-09 21:37:05 +0200

    wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,36 +4,6 @@ module.exports = {
   stories: ['../src/components/**/*.stories.[tj]s'],
   addons: ['@storybook/addon-knobs/register'],
   webpackFinal: async (config, { configType }) => {
-    config.module.rules.push(
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../src'),
-      },
-      {
-        test: /\.pcss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    'postcss-preset-env',
-                    {
-                      // Options
-                    },
-                  ],
-                ],
-              },
-            },
-          },
-        ],
-      },
-    );
-
     return config;
   },
 };

3bd18b863654ae7763357fc01873cd379efc68f3
leifssm <lofftull@gmail.com>
2020-10-09 21:50:17 +0200

    wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
+const custom = require('../webpack.config.js');
+
 module.exports = {
   stories: ['../src/components/**/*.stories.[tj]s'],
   addons: ['@storybook/addon-knobs/register'],
   webpackFinal: async (config, { configType }) => {
-    return config;
+    return { ...config, module: { ...config.module, rules: custom.module.rules } };
   },
 };
